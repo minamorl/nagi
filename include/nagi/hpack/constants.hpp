7f596a2855b7f0a06b423ccdc37e064233156a68
minamorl <minamorl@minamorl.com>
2019-04-08 21:03:54 +0900

    Add STATIC_TABLE

--- include/nagi/hpack/constants.hpp
+++ include/nagi/hpack/constants.hpp
@@ -5,6 +5,7 @@
 
 namespace nagi { namespace hpack {
 struct constants {
+    static const std::vector<std::pair<std::string, std::string>> STATIC_TABLE;
     static const std::vector<std::pair<uint32_t, uint8_t>> HUFFMAN_CODE_TABLE;
 };
 } }// nagi 
